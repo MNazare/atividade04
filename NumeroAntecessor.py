1edc4f7c04d578bd8cda4f57c3d34635915ffa85
MNazare <flautamortal@gmail.com>
2023-10-16 15:09:23 -0300

    resolução da atividade 04

---
+++ NumeroAntecessor.py
@@ -0,0 +1,8 @@
+# Escreva um numero e veja qual o seu antecessor e seu sucessor! 
+import time
+numero = int(input("numero:"))
+antecessor = (numero-1)
+print("o antecessor de", numero, "é:", antecessor)
+sucessor = (numero+1)
+time.sleep(2)
+print("o sucessor de", numero, "é:", sucessor )
